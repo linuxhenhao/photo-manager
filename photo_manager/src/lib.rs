52f104d39cee1e7162ac62f24a14d8b5d305dc8a
huangyu <diwang90@gmail.com>
2019-08-11 23:39:25 +0800

    WIP: file iterator

--- photo_manager/src/lib.rs
+++ photo_manager/src/lib.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 use std::fs;
+use std::iter::Iterator;
+
 use mime_guess;
 
 struct Indexer {
@@ -17,8 +19,35 @@ struct OneIndex {
     checksum_type: ChecksumType,
 }
 
+struct FileIter {
+    root_path: &Path,
+    genera_iter: Iterator,
+}
+
+impl FileIter {
+    pub fn new(root_path: &Path) -> FileIter {
+        Filter {root_path, root_path.iter()}
+    }
+}
+
+impl Iterator for FileIter {
+    type Item = Path;
+
+    fn next(&mut self) -> Option(Path) {
+        if let Some(item_path) = self.genera_iter.next() {
+            if item_path.is_dir() {
+                find_images
+            } else if item_path.is_file() {
+                // ignore symlink file
+                fs::metadata(item_path.to
+            }
+        }
+    }
+}
+
+
 impl Indexer {
-    fn build(&self, ) -> vec<OneIndex> {
+    fn build(&self) -> vec<OneIndex> {
         let mut indexes = Vec::new();
         self.root_dir;
         let one_index = OneIndex {
@@ -30,16 +59,10 @@ impl Indexer {
         indexes
     }
 
-    fn get_all_file_paths(dir_path: &Path) -> Vec<Path> {
+    fn file_path_iter(&self) -> Iterator<Path> {
+        let root_path = Path::new(self.root_dir);
         let mut results = Vec::new();
-        for item_path in dir_path.iter() {
-            if item_path.is_dir() {
-                find_images
-            } else if item_path.is_file() {
-                // ignore symlink file
-                fs::metadata(item_path.to
-            }
-        }
+        
     }
 }
     
