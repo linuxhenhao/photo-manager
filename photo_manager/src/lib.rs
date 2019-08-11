c6fb4a0dd2b47d4e4f3a1907c19546dd00947327
huangyu <diwang90@gmail.com>
2019-08-11 23:12:48 +0800

    WIP

--- photo_manager/src/lib.rs
+++ photo_manager/src/lib.rs
@@ -2,22 +2,49 @@ use std::path::Path;
 use std::fs;
 use mime_guess;
 
-fn build_index(dir_path: &str) {
-    
+struct Indexer {
+    root_dir: String,
 }
 
+enum ChecksumType {
+    MD5
+}
+
+struct OneIndex {
+    // relative path
+    path: String,
+    checksum: String,
+    checksum_type: ChecksumType,
+}
+
+impl Indexer {
+    fn build(&self, ) -> vec<OneIndex> {
+        let mut indexes = Vec::new();
+        self.root_dir;
+        let one_index = OneIndex {
+            "test",
+            "flkajfklajkler",
+            ChecksumType::MD5
+        };
+        indexes.push(one_index);
+        indexes
+    }
+
+    fn get_all_file_paths(dir_path: &Path) -> Vec<Path> {
+        let mut results = Vec::new();
+        for item_path in dir_path.iter() {
+            if item_path.is_dir() {
+                find_images
+            } else if item_path.is_file() {
+                // ignore symlink file
+                fs::metadata(item_path.to
+            }
+        }
+    }
+}
+    
 fn find_image(dir_path: &Path) -> Vec<Path> {
 }
 
 
-fn get_all_file_paths(dir_path: &Path) -> Vec<Path> {
-    let mut results = Vec::new();
-    for item_path in dir_path.iter() {
-        if item_path.is_dir() {
-            find_images
-        } else if item_path.is_file() {
-            // ignore symlink file
-            fs::metadata(item_path.to
-        }
-    }
-}
+
